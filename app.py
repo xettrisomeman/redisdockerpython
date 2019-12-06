ad4144c2a8de51b82d0e16c316a3279f468180ad
webizfabulous <webizmoxx@gmail.com>
2019-12-06 18:32:47 +0545

    Added all the files

---
+++ app.py
@@ -0,0 +1,17 @@
+import redis 
+
+
+
+def hello_redis():
+
+    redisClient = redis.Redis()
+
+    datas = {"name"  : "johncena" , "age" : 22, "country" : "china"}
+
+    redisClient.hmset("stu:101" , datas)
+
+    print(redisClient.hgetall("stu:101"))
+
+
+
+hello_redis()
